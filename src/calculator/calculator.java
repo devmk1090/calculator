b04faa41bf41f52a59868445e5a6d094bc86120c
devmk <devmk@DESKTOP-DIB6VRN>
2019-07-20 17:35:18 +0900

    Rev1

--- src/calculator/calculator.java
+++ src/calculator/calculator.java
@@ -6,16 +6,25 @@ public class calculator {
 		System.out.println("첫 번째 입력 값 : ");
 		int first = scanner.nextInt();
 		System.out.println(first);
+
+		System.out.println("사칙연산 : ");
+		String third = scanner.next();
+		System.out.println(third);
 		
 		System.out.println("두 번째 입력 값 : ");
 		int second = scanner.nextInt();
 		System.out.println(second);
 		
-		System.out.println("더하기 : " + (first+second));
-		System.out.println("빼기 : " + (first-second));
-		System.out.println("나누기 : " + (first/second));
-		System.out.println("곱하기 : " + (first*second));
-		
+		if (third.contentEquals("+")) {
+			System.out.println("더하기 :" + (first+second));
+		} else if(third.contentEquals("-")) {
+			System.out.println("빼기 : " + (first-second));
+		} else if(third.contentEquals("/")) {
+			System.out.println("나누기 : " + (first/second));
+		} else if (third.contentEquals("*")) {
+			System.out.println("곱하기 : " + (first*second));
+		} else {
+			System.out.println("사칙연산 기호가 아닙니다.");
+		}
 	}
-
 }
